f450467e79063f1895c5fe8ba0327845ee479c0f
essu christian <essuchristian@gmail.com>
2025-08-30 14:02:37 -0700

    Task 2: Added Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,17 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
+
 function App() {
   return (
     <div>
+      <Header />
       <WelcomeMessage />
+      <MainContent />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
