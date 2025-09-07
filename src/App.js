bab14a6f585ec1a0eacfdda9ac1de39e82e19c4a
nobleslim24 <essuchristian@gmail.com>
2025-09-07 16:57:38 +0100

    Task 0 - Inline CSS styling applied

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
+import React from 'react';
 import Header from './components/Header';
 import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
-
 function App() {
   return (
     <div>
       <Header />
-      <UserProfile 
-        name="Essu Christian" 
-        age={27} 
-        bio="A passionate front-end developer and WordPress enthusiast." 
-      />
+      <UserProfile name="Essu Christian" age={27} bio="A passionate front-end developer and WordPress enthusiast." />
       <MainContent />
       <Footer />
     </div>
