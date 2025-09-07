f76b8e4289cba24d61d2a9a16d72a93288ff770c
nobleslim24 <essuchristian@gmail.com>
2025-09-08 00:55:16 +0100

    iniitial commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function MainContent() {
+    return(
+  <main style={{ padding: '10px', backgroundColor: '#913636ff'}}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+    );
+}
+export default MainContent;
