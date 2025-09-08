5ba2268a0ee38453bb274d7d41e09e0703b8e8b1
nobleslim24 <essuchristian@gmail.com>
2025-09-08 03:02:55 +0100

    initial commit

--- alx-react-app -props/src/UserDetails.jsx
+++ alx-react-app -props/src/UserDetails.jsx
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 import UserContext from "./UserContext";
 function UserDetails() {
-  const data = useContext(UserContext);
+  const userData = useContext(UserContext);
   return (
     <div>
-      <p>Name: {data.name}</p>
-      <p>Email: {data.email}</p>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
