fa7afc31bbcc9bc31c0449e98a736d4afa307308
nobleslim24 <essuchristian@gmail.com>
2025-09-09 23:24:09 +0100

    updated commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+import EditRecipeForm from "./components/EditRecipeForm";
+import DeleteRecipeButton from "./components/DeleteRecipeButton";
 
 
 function App() {
   return (
-    <div>
+    <>
+    <BrowserRouter>
+      
+      <Routes>
+        <Route path="/" element={<RecipeDetails />} />
+        <Route path="/recipe" element={<EditRecipeForm/>} />
+        <Route path="/recipe.id" element={<DeleteRecipeButton />} />
+      
+        
+      </Routes>
+    </BrowserRouter>
+    
+     <div>
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
       <RecipeList />
-     
-    </div>
+      </div>
+      </>
+      
+    
   );
 }
 
