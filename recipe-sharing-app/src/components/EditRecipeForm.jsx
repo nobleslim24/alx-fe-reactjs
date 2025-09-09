292d35765599037724fd3bedb955cb2a95acb342
nobleslim24 <essuchristian@gmail.com>
2025-09-09 23:34:41 +0100

    updated commit

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useRecipeStore } from './recipeStore';
 
-const EditRecipeForm = ({ recipe }) => {
+export const EditRecipeForm = ({ recipe }) => {
   const updateRecipe = useRecipeStore((state) => state.updateRecipe);
   const [title, setTitle] = useState(recipe.title);
   const [description, setDescription] = useState(recipe.description);
@@ -20,4 +20,3 @@ const EditRecipeForm = ({ recipe }) => {
   );
 };
 
-export default EditRecipeForm;
