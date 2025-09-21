19fd3a740a6d3d2e36ae94a90e915e93f67d765a
nobleslim24 <essuchristian@gmail.com>
2025-09-21 15:57:55 +0100

    Task 0: Setup GitHub User Search React project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>GitHub User Search App</h1>
+      <p>Project setup successful 🎉</p>
+    </div>
+  );
+}
+
+export default App;
