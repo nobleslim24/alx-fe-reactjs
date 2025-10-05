27974fd5b1f645c902928479225eb1a72d51987b
nobleslim24 <essuchristian@gmail.com>
2025-10-05 23:41:35 +0100

    updated commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,20 +1,22 @@
+import React from "react";
 
-function UserProfile() {
+const UserProfile = () => {
   return (
-    <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-white shadow-md rounded-lg sm:p-4 md:p-8 mx-auto max-w-xs md:max-w-sm mt-10 text-center">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full mx-auto mb-4 w-24 h-24 md:w-36 md:h-36 object-cover"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 font-semibold my-4">
-        John Doe
+      <h1 className="text-lg md:text-xl font-semibold text-gray-800">
+        Christian Essu
       </h1>
-      <p className="text-gray-600 text-sm md:text-base">
-        Developer at Example Co. Loves to write code and explore new technologies.
+      <p className="text-sm md:text-base text-gray-600">
+        Frontend Developer & Designer passionate about creating responsive web
+        experiences.
       </p>
     </div>
   );
-}
+};
 
 export default UserProfile;
