ef3e5c49d513b467d14a3a90bf318e3aa5b7d0e6
nobleslim24 <essuchristian@gmail.com>
2025-10-19 16:12:00 +0100

    updated commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,39 +1,31 @@
-import React, { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const [isAuthenticated, _setIsAuthenticated] = useState(true);
-
-
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/blog/123">Blog Post (ID 123)</Link>
-      </nav>
-
+    <BrowserRouter>
       <Routes>
+      
         <Route path="/" element={<Home />} />
+
+      
+        <Route path="/blog/:id" element={<BlogPost />} />
 
         
         <Route
           path="/profile/*"
           element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
+            <ProtectedRoute>
               <Profile />
             </ProtectedRoute>
           }
         />
-
-        
-        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import useAuth from "../auth/useAuth";
 
-function ProtectedRoute({ isAuthenticated, children }) {
-  if (!isAuthenticated) {
-    return <Navigate to="/" replace />;
-  }
-  return children;
-}
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = useAuth(); 
+
+  return isAuthenticated ? children : <Navigate to="/" />;
+};
 
 export default ProtectedRoute;
