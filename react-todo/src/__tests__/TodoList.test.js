2bde1af883787f987202f6f36688a2f33dcc01fa
nobleslim24 <essuchristian@gmail.com>
2025-10-19 23:07:45 +0100

    updated commit

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent,} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import TodoList from "../components/TodoList";
 import { test, expect } from "vitest";
+
+const sum = (a, b) => a + b;
+
+test("adds 2 + 3 to equal 5", () => {
+  expect(sum(2, 3)).toBe(5);
+});
 
 
 test("renders initial todos", () => {
@@ -29,6 +35,6 @@ test("toggles a todo", () => {
 
 test("deletes a todo", () => {
   render(<TodoList />);
-  fireEvent.click(screen.getByText("Delete"));
+  fireEvent.click(screen.getAllByText("Delete")[0]);
   expect(screen.queryByText("Learn React")).toBeNull();
 });
