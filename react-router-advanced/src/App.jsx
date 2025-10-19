4e61781d40fb602bdd1e5ead8df78b322adf5071
nobleslim24 <essuchristian@gmail.com>
2025-10-19 16:06:19 +0100

    updated commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,25 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  const [isAuthenticated, _setIsAuthenticated] = useState(true);
+
+
   return (
-    <BrowserRouter>
+    <div>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/blog/123">Blog Post (ID 123)</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
+
+        
         <Route
           path="/profile/*"
           element={
-            <ProtectedRoute>
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
               <Profile />
             </ProtectedRoute>
           }
         />
-        <Route path="/post/:id" element={<BlogPost />} />
+
+        
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
